5219ae29261d2434842fb44662266a427fb3edea
chiragsharma7495 <chiragsharma7495@gmail.com>
2026-07-05 13:05:28 +0530

    "created gradle project for QuoraApiConstruction"

---
+++ src/main/java/org/example/quoraapiconstruction/QuoraApiConstructionApplication.java
@@ -0,0 +1,13 @@
+package org.example.quoraapiconstruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoraApiConstructionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoraApiConstructionApplication.class, args);
+    }
+
+}
